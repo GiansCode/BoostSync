5e76e2bcc628c525d98c3be41ca52c52d2e45537
Gabriel Dumitru <contactgabytm@gmail.com>
2022-08-27 13:54:06 +0300

    update bot code to be compatible with the latest JDA

--- bot/src/main/java/io/samdev/boostsync/bot/BoostSync.java
+++ bot/src/main/java/io/samdev/boostsync/bot/BoostSync.java
@@ -3,120 +3,107 @@ package io.samdev.boostsync.bot;
 import io.samdev.boostsync.bot.boost.BoostManager;
 import io.samdev.boostsync.bot.command.CommandListener;
 import io.samdev.boostsync.common.database.BoostSyncDatabase;
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import static io.samdev.boostsync.bot.util.Logging.info;
 import static io.samdev.boostsync.bot.util.Logging.severe;
 
-public class BoostSync
-{
-	public static void main(String[] args)
-	{
-		new BoostSync();
-	}
+public class BoostSync {
+    public static void main(String[] args) {
+        new BoostSync();
+    }
 
-	private BoostSync()
-	{
-		if (!loadConfig())
-		{
-			severe("Unable to load config");
-			return;
-		}
+    private BoostSync() {
+        if (!loadConfig()) {
+            severe("Unable to load config");
+            return;
+        }
 
-		if (!connectToDatabase())
-		{
-			severe("Unable to connect to database");
-			return;
-		}
+        if (!connectToDatabase()) {
+            severe("Unable to connect to database");
+            return;
+        }
 
-		if (!connectToDiscord())
-		{
-			severe("Unable to connect to Discord");
-		}
+        if (!connectToDiscord()) {
+            severe("Unable to connect to Discord");
+        }
 
-		new CommandListener(this);
-		boostManager = new BoostManager(this);
-	}
+        new CommandListener(this);
+        boostManager = new BoostManager(this);
+    }
 
-	private BoostManager boostManager;
+    private BoostManager boostManager;
 
-	public BoostManager getBoostManager()
-	{
-		return boostManager;
-	}
+    public BoostManager getBoostManager() {
+        return boostManager;
+    }
 
-	private BotConfig config;
+    private BotConfig config;
 
-	public BotConfig getConfig()
-	{
-		return config;
-	}
+    public BotConfig getConfig() {
+        return config;
+    }
 
-	private boolean loadConfig()
-	{
-		config = new BotConfig(this);
-		return config.init();
-	}
+    private boolean loadConfig() {
+        config = new BotConfig(this);
+        return config.init();
+    }
 
-	private BoostSyncDatabase database;
+    private BoostSyncDatabase database;
 
-	public BoostSyncDatabase getDatabase()
-	{
-		return database;
-	}
+    public BoostSyncDatabase getDatabase() {
+        return database;
+    }
 
-	private boolean connectToDatabase()
-	{
-		try
-		{
-			database = new BoostSyncDatabase(config.getDatabaseCredentials());
-			return true;
-		}
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-			return false;
-		}
-	}
+    private boolean connectToDatabase() {
+        try {
+            database = new BoostSyncDatabase(config.getDatabaseCredentials());
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
 
-	private JDA jda;
+    private JDA jda;
 
-	public JDA getJda()
-	{
-		return jda;
-	}
+    public JDA getJda() {
+        return jda;
+    }
 
-	private Guild guild;
+    private Guild guild;
 
-	public Guild getGuild()
-	{
-		return guild;
-	}
+    public Guild getGuild() {
+        return guild;
+    }
 
-	private boolean ready = false;
+    private boolean ready = false;
 
-	private boolean connectToDiscord()
-	{
-		try
-		{
-			jda = new JDABuilder(AccountType.BOT)
-				.setToken(config.getBotToken())
-				.build();
+    private boolean connectToDiscord() {
+        try {
+            jda = JDABuilder.create(
+                            GatewayIntent.GUILD_MEMBERS,
+                            GatewayIntent.GUILD_MESSAGES,
+                            GatewayIntent.MESSAGE_CONTENT
+                    )
+                    .disableCache(CacheFlag.ACTIVITY, CacheFlag.EMOJI, CacheFlag.CLIENT_STATUS, CacheFlag.ONLINE_STATUS, CacheFlag.STICKER, CacheFlag.VOICE_STATE)
+                    .setToken(config.getBotToken())
+                    .build();
 
-			jda.awaitReady();
+            jda.awaitReady();
 
-			info("Connected to bot " + jda.getSelfUser().getAsTag());
-			guild = jda.getGuildById(config.getGuildId());
+            info("Connected to bot " + jda.getSelfUser().getAsTag());
+            guild = jda.getGuildById(config.getGuildId());
 
-			return true;
-		}
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-			return false;
-		}
-	}
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
 }
